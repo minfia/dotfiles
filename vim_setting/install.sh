99d2e39c4de018e159d5462ef68d48bfa001cf97
minfia <minfialab@gmail.com>
2021-10-08 17:33:15 +0900

    vim_setting: インストール時の環境選択で、プログラミング言語なしを追加

--- vim_setting/install.sh
+++ vim_setting/install.sh
@@ -2,12 +2,16 @@
 
 
 COMPANY_FLAG=0
+NO_LANG_FLAG=0
 
 for OPT in "$@"
 do
   case $OPT in
     company )
       COMPANY_FLAG=1;
+      ;;
+    no-lang )
+      NO_LANG_FLAG=1;
       ;;
     * )
       echo "option error. (input: $OPT)"
@@ -33,7 +37,9 @@ function main()
 
   package_install
 
-  install_gnu_global
+  if [ $NO_LANG_FLAG -eq 0 ]; then
+    install_gnu_global
+  fi
 
   # deinのインストール
   if [ ! -d ~/.vim/dein ]; then
@@ -46,7 +52,7 @@ function main()
 
   # プラグインのインストール
   vim -c ":q"
-  if [ $COMPANY_FLAG -eq 0 ]; then
+  if [ $COMPANY_FLAG -eq 0 ] && [ $NO_LANG_FLAG -eq 0 ]; then
     cd ~/.vim/dein/repos/github.com/iamcco/markdown-preview.nvim/app
     ./install.sh
   fi
@@ -158,18 +164,21 @@ function set_package_management_system_at_distribution()
 # 環境構築に必要なパッケージのインストール
 function package_install()
 {
+  install_pkg $PKG_MNG_SYS "vim"
   install_pkg $PKG_MNG_SYS "curl"
   install_pkg $PKG_MNG_SYS "wget"
-  install_pkg $PKG_MNG_SYS "gcc"
-  install_pkg $PKG_MNG_SYS "make"
-  install_pkg $PKG_MNG_SYS "libncurses5-dev"
-  install_pkg $PKG_MNG_SYS "clangd"
-  install_pkg $PKG_MNG_SYS "vim"
   install_pkg $PKG_MNG_SYS "git"
-  install_pkg $PKG_MNG_SYS "python3"
-  install_pkg $PKG_MNG_SYS "python3-pip"
-  install_pkg $PKG_MNG_SYS "python3-venv"
-  install_pkg "pip3" "python-language-server"
+
+  if [ $NO_LANG_FLAG -eq 0 ]; then
+    install_pkg $PKG_MNG_SYS "gcc"
+    install_pkg $PKG_MNG_SYS "make"
+    install_pkg $PKG_MNG_SYS "libncurses5-dev"
+    install_pkg $PKG_MNG_SYS "clangd"
+    install_pkg $PKG_MNG_SYS "python3"
+    install_pkg $PKG_MNG_SYS "python3-pip"
+    install_pkg $PKG_MNG_SYS "python3-venv"
+    install_pkg "pip3" "python-language-server"
+  fi
 }
 
 # Gnu GLOBALのインストール
@@ -194,11 +203,65 @@ function install_vim_setting()
   cp -r .vim/ ~/
   mkdir ~/.vim/rc
 
-  cp ./toml/*.* ~/.vim/rc/
+  local LANG_TOML_LIST=("lsp.toml" "web.toml" "markdown.toml")
+  local EXCLUDE_LIST=
+  local CP_TOML_LIST=
+
+  if [ $NO_LANG_FLAG -eq 1 ]; then
+    # 除外リスト生成
+    for FE in ${LANG_TOML_LIST[@]}
+    do
+      EXCLUDE_LIST=$EXCLUDE_LIST" -not -name $FE"
+    done
+
+    # .vimrcで除外するものを編集
+
+    # GNU GLOBAL関連
+    local VIMRC=~/.vimrc
+    GGL=`sed -n '/GNU GLOBAL/=' $VIMRC`
+    sed -i ${GGL},`expr $GGL + 6`d $VIMRC
+
+    # toml関連
+    local DEL_TOML_LIST=${LANG_TOML_LIST[@]//./_}
+
+    for DT in $DEL_TOML_LIST
+    do
+      DTNL=`sed -n /${DT}/= $VIMRC`
+      DTNL_R=`reverse_array ${DTNL[@]}`
+      for DTN in $DTNL_R
+      do
+        sed -i ${DTN}d $VIMRC
+      done
+    done
+
+  fi
+
+  # companyディレクトリと除外リストは除外して、ファイル検索し、ファイルリストを取得
+  CP_TOML_LIST=`find ./toml/ -type d -name company -prune -o -type f $EXCLUDE_LIST -print`
+
+  cp $CP_TOML_LIST ~/.vim/rc/
   if [ $COMPANY_FLAG -eq 1 ]; then
     cp -f ./toml/company/*.* ~/.vim/rc/
   fi
   mkdir ~/.vim/plugin
+}
+
+# 配列の中身を逆順にする
+function reverse_array()
+{
+  local ARR=($@)
+  local ARR_SIZE=${#ARR[@]}
+  for ((i=0; i<$ARR_SIZE; i++)); do
+    if [ $i -ge `expr $ARR_SIZE / 2` ]; then
+      break
+    fi
+    TEMP=${ARR[i]}
+
+    ARR[i]=${ARR[$ARR_SIZE-1-i]}
+    ARR[$ARR_SIZE-1-i]=$TEMP
+  done
+
+  echo ${ARR[@]}
 }
 
 
